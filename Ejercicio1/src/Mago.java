6e241a65d5c85cbe864fd06406391385a9fd1d20
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2024-08-08 18:56:25 -0500

    Arreglar heroes

--- Ejercicio1/src/Mago.java
+++ Ejercicio1/src/Mago.java
@@ -1,12 +1,2 @@
-public class Guerrero extends Heroe {
-
-    public Guerrero(String name, String equipment, int experience, int level, int skillPoints){
-        super(name, equipment, experience, level, skillPoints);
-    }
-
-
-    @Override
-    public Heroe clone() {
-        return new Guerrero(this.name, this.equipment, this.experience, this.level, this.skillPoints);
-    }
+package PACKAGE_NAME;public class Mago {
 }
